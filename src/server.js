2d5f2b4a28137c72aef96a5e4ad92819d01c347a
Victor <vn28007@gmail.com>
2020-06-06 11:12:11 -0300

    Routes setted on localhost (server.js)

--- src/server.js
+++ src/server.js
@@ -1,12 +1,25 @@
 const express = require("express")
 const server = express()
 
-//configurar caminhos da aplicação
+//configurar pasta public
+server.use(express.static("public"))
+
+//configurar caminhos (rotas) da aplicação
+
 //página inicial
 //request and response
 server.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html")
 })
+//create-point
+server.get("/create-point", (req, res) => {
+    res.sendFile(__dirname + "/views/create-point.html")
+})
+//search-results
+server.get("/search-results", (req, res) => {
+    res.sendFile(__dirname + "/views/search-results.html")
+})
+
 
 //ligar o servidor
 server.listen(3000)
