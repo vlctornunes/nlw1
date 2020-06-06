320c95b37cd521aaceffb92998a442776a67ac2c
Victor <vn28007@gmail.com>
2020-06-06 11:01:13 -0300

    Nodemon dependence added. Server open index

--- src/server.js
+++ src/server.js
@@ -1,5 +1,12 @@
 const express = require("express")
 const server = express()
 
+//configurar caminhos da aplicação
+//página inicial
+//request and response
+server.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html")
+})
+
 //ligar o servidor
 server.listen(3000)
